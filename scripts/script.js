1a09647b8bafe2e67cb27d6e9f45761397eb9dcc
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-06-23 17:20:30 -0300

    commit js

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ const obj_p_rede_social = document.querySelector('#p_rede_social')
 
 obj_sel_rede_social.addEventListener('change', FunMudaFigura)
 
-function  FunMudaFigura() {
-    obj_img_rede_social.setAttribute('src', 'imagens/' + obj_sel_rede_social.value + '.png' ) // ok
-    obj_img_rede_social.setAttribute('alt','Logo ' + obj_sel_rede_social.value ) // 
-    obj_img_rede_social.setAttribute('title','Logo ' + obj_sel_rede_social.value )
+function FunMudaFigura() {
+    obj_img_rede_social.setAttribute('src', 'imagens/' + obj_sel_rede_social.value + '.png') // ok
+    obj_img_rede_social.setAttribute('alt', 'Logo ' + obj_sel_rede_social.value) // 
+    obj_img_rede_social.setAttribute('title', 'Logo ' + obj_sel_rede_social.value)
     obj_p_rede_social.innerText = 'Logo ' + obj_sel_rede_social.value
 }
 
@@ -21,24 +21,102 @@ const obj_div_adiciona_figura = document.querySelector('#div_adiciona_figura')
 const obj_img_nova = document.createElement('img')
 const obj_input_novo = document.createElement('input')
 
-for (obj_img of obj_figuras_array){
-    obj_img.addEventListener('click', function() { FunAdicionaFigura(this.src, this.alt) } )
+for (obj_img of obj_figuras_array) {
+    obj_img.addEventListener('click', function () { FunAdicionaFigura(this.src, this.alt) })
 }
 
-function  FunAdicionaFigura(par_src, par_alt) { //par = parametro
+function FunAdicionaFigura(par_src, par_alt) { //par = parametro
     obj_img_nova.setAttribute('src', par_src)
     obj_img_nova.setAttribute('alt', par_alt)
     obj_img_nova.setAttribute('title', par_alt)
-    obj_div_adiciona_figura.appendChild(obj_img_nova) 
+    obj_div_adiciona_figura.appendChild(obj_img_nova)
+
+    // input
 
     obj_input_novo.setAttribute('type', 'text')
     obj_input_novo.setAttribute('desabled', 'true')
     obj_input_novo.setAttribute('value', par_alt)
-    obj_input_novo.setAttribute('class','novo_input')
-    obj_div_adiciona_figura.appendChild(obj_input_novo) 
+    obj_input_novo.setAttribute('class', 'novo_input')
+    obj_div_adiciona_figura.appendChild(obj_input_novo)
+}
 
+// Preencher Tabela
+const obj_td_array = document.querySelectorAll('#tab_principal td')
+const obj_tab_vazia = document.querySelector('#tab_vazia')
+const obj_caption_vazia = document.querySelector('#tab_vazia caption')
+const obj_prog_tab_vazia = document.querySelector('#prog_tab_vazia')
+
+let contador_linhas = 0
+
+for (obj_td of obj_td_array) {
+    obj_td.addEventListener('click', function () { FunPreencheTabela(this.innerText) })
+}
+
+function FunPreencheTabela(par_text) {
+    if (contador_linhas < 5) {
+        const obj_tr_novo = document.createElement('tr')
+        const obj_td_novo = document.createElement('td')
+        obj_td_novo.innerText = par_text
+        obj_tr_novo.appendChild(obj_td_novo)
+        obj_tab_vazia.appendChild(obj_tr_novo)
+        contador_linhas++
+        obj_caption_vazia.innerHTML = `Tabela com ${contador_linhas} Linha(s)`
+        obj_prog_tab_vazia.value = contador_linhas
+    } else {
+        alert('Tabela Cheia!')
+    }
+}
+// Segunda parte - Mudar a Figura
+
+const obj_rb_array = document.querySelectorAll('[name="redessociais"]')
+const obj_img_code = document.querySelector('#img_code')
+const obj_pre = document.querySelector('#predefinido')
+
+for (obj_rb of obj_rb_array) {
+    obj_rb.addEventListener('click', FunMudaImgLingProg)
 }
 
 
+function FunMudaImgLingProg() {
+    for (obj_rb of obj_rb_array) {
+        if (obj_rb.checked) {
+            obj_img_code.setAttribute('src', 'imagens/' + obj_rb.value + '.png')
+            obj_img_code.setAttribute('alt', 'Logo ' + obj_rb.value)
+            obj_img_code.setAttribute('title', 'Logo ' + obj_rb.value)
+            obj_pre.innerText = 'Logo ' + obj_rb.value
+        }
+    }
+}
 
+// Preencher Lista
+
+
+
+/*
+const obj_td_array = document.querySelectorAll('#tab_principal td')
+const obj_tab_vazia = document.querySelector('#tab_vazia')
+const obj_caption_vazia = document.querySelector('#tab_vazia caption')
+const obj_prog_tab_vazia = document.querySelector('#prog_tab_vazia')
+
+let contador_linhas = 0
+
+for (obj_td of obj_td_array) {
+    obj_td.addEventListener('click', function () { FunPreencheTabela(this.innerText) })
+}
+
+function FunPreencheTabela(par_text) {
+    if (contador_linhas < 5) {
+        const obj_tr_novo = document.createElement('tr')
+        const obj_td_novo = document.createElement('td')
+        obj_td_novo.innerText = par_text
+        obj_tr_novo.appendChild(obj_td_novo)
+        obj_tab_vazia.appendChild(obj_tr_novo)
+        contador_linhas++
+        obj_caption_vazia.innerHTML = `Tabela com ${contador_linhas} Linha(s)`
+        obj_prog_tab_vazia.value = contador_linhas
+    } else {
+        alert('Tabela Cheia!')
+    }
+}
+*/
 
